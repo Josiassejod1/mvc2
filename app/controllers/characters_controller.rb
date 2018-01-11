a62fbbf6909a6bcab60b42521fe03ba4f0d49478
Dalvin <army77badboy@gmail.com>
2018-01-11 19:25:04 +0000

    Made changes to errors in csv file, naming convention, movelist, and added missing images for Jill & SonSon

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,6 +8,7 @@ class CharactersController < ApplicationController
     def show
        @I = Character.where({name: params[:id]}).first
        #@moves = Move.where({character_name: 'Akuma'})
+       #This method was used to account for white space when looking for a characters name
        @moves = Move.where("character_name like ?", "%#{params[:id]}%")
      # puts @moves
      
